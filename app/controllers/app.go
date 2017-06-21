29865781a934c116ab802ba68c0b30aa93ec4d13
yash-sri <yash.srivastava@homebell.com>
2017-06-21 21:34:52 +0530

    fixed imports

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,17 +1,16 @@
 package controllers
 
 import (
-	"github.com/revel/revel"
-	"github.com/iot_platform/lib/sender"
-	"github.com/iot_platform/conf"
-	"github.com/iot_platform/lib/utils"
 	"github.com/StabbyCutyou/buffstreams"
+	"github.com/iot_platform/conf"
+	"github.com/iot_platform/lib/sender"
+	"github.com/iot_platform/lib/utils"
+	"github.com/revel/revel"
 )
 
 type App struct {
 	*revel.Controller
 }
-
 
 func (c App) Index() revel.Result {
 	return c.Render()
@@ -19,10 +18,10 @@ func (c App) Index() revel.Result {
 
 func (c App) Send_3000() revel.Result {
 
-	response := sender.Response{Success: false, Message:"Something went wrong"}
+	response := sender.Response{Success: false, Message: "Something went wrong"}
 	packet := sender.Packet_3000{}
 	err := c.Params.BindJSON(&packet)
-	if err==nil{
+	if err == nil {
 		response = sender.HandlePacket(0x3000, packet)
 	}
 	return c.RenderJSON(response)
@@ -30,21 +29,21 @@ func (c App) Send_3000() revel.Result {
 
 func (c App) Send_8000() revel.Result {
 
-	response := sender.Response{Success: false, Message:"Something went wrong"}
+	response := sender.Response{Success: false, Message: "Something went wrong"}
 	packet := sender.Packet_8000{}
 	err := c.Params.BindJSON(&packet)
-	if err==nil{
+	if err == nil {
 		response = sender.HandlePacket(0x8000, packet)
 	}
 	return c.RenderJSON(response)
 }
 
 func (c App) IsSguConnected() revel.Result {
-	response := sender.Response{Success: false, Message:"Something went wrong"}
+	response := sender.Response{Success: false, Message: "Something went wrong"}
 
 	params := make(map[string]uint64)
 	err := c.Params.BindJSON(&params)
-	if err==nil{
+	if err == nil {
 		response.Data = conf.SGU_TCP_CONNECTION.Has(utils.ToStr(params["sguid"]))
 		response.Success = true
 		response.Message = ""
@@ -53,13 +52,13 @@ func (c App) IsSguConnected() revel.Result {
 }
 
 func (c App) GetConnectedScus() revel.Result {
-	response := sender.Response{Success: false, Message:"Something went wrong"}
+	response := sender.Response{Success: false, Message: "Something went wrong"}
 
 	params := make(map[string]uint64)
 	err := c.Params.BindJSON(&params)
-	if err==nil{
+	if err == nil {
 
-		response.Data,_ = conf.SGU_SCU_LIST.Get(utils.ToStr(params["sguid"]))
+		response.Data, _ = conf.SGU_SCU_LIST.Get(utils.ToStr(params["sguid"]))
 		response.Success = true
 		response.Message = ""
 	}
@@ -70,16 +69,16 @@ func (c App) GetConnectedSgus() revel.Result {
 	data := conf.Sgu{}
 	var sgus []uint64
 	if buffstreams.TcpSguMap != nil {
-		for _,v := range (buffstreams.TcpSguMap.Items()){
-			if v!= nil{
-				sgus = append(sgus,utils.ToUint64(v))
+		for _, v := range buffstreams.TcpSguMap.Items() {
+			if v != nil {
+				sgus = append(sgus, utils.ToUint64(v))
 			}
 		}
 	}
 
 	data.SguIds = sgus
 	response := sender.Response{}
-	response.Data= data
+	response.Data = data
 	response.Success = true
 
 	return c.RenderJSON(response)
@@ -87,10 +86,10 @@ func (c App) GetConnectedSgus() revel.Result {
 
 func (c App) Get() revel.Result {
 
-	response := sender.Response{Success: true, Message:"Something went wrong"}
+	response := sender.Response{Success: true, Message: "Something went wrong"}
 	packet := make(map[string]interface{})
 	err := c.Params.BindJSON(&packet)
-	if err==nil{
+	if err == nil {
 		revel.INFO.Println(packet)
 	}
 	return c.RenderJSON(response)
